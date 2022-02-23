7ff49ec5c82a746c41959a9ce24f3cf6b4f1e326
L1yb <1747848577@qq.com>
2022-02-23 17:08:46 +0800

    complete

--- rcssbase/net/socketstreambuf.hpp
+++ rcssbase/net/socketstreambuf.hpp
@@ -18,6 +18,7 @@
  *                                                                         *
  ***************************************************************************/
 
+
 #ifndef RCSS_NET_SOCKETSTREAMBUF_HPP
 #define RCSS_NET_SOCKETSTREAMBUF_HPP
 
@@ -27,75 +28,76 @@
 //#include <streambuf>
 #include <iostream>
 
-namespace rcss
-{
-    namespace net
-    {
+namespace rcss {
+namespace net {
 
-        class Socket;
+class Socket;
 
-        class SocketStreamBuf
-            : public std::streambuf
-        {
-        public:
-            //     typedef int int_type;
-            typedef char char_type;
+class SocketStreamBuf
+    : public std::streambuf {
+public:
+//     typedef int int_type;
+    typedef char char_type;
 
-            enum ConnType
-            {
-                CONN_ON_READ,
-                NO_CONN
-            };
+    enum ConnType { CONN_ON_READ, NO_CONN };
 
-        private:
-            Socket &M_socket;
-            Addr M_end_point;
-            std::streamsize M_bufsize;
-            char_type *M_inbuf;
-            char_type *M_outbuf;
-            int M_remained;
-            char_type M_remained_char;
-            ConnType M_connect;
+private:
 
-            // not used
-            SocketStreamBuf(const SocketStreamBuf &);
-            // not used
-            SocketStreamBuf &operator=(const SocketStreamBuf &);
+    Socket & M_socket;
+    Addr M_end_point;
+    std::streamsize M_bufsize;
+    char_type * M_inbuf;
+    char_type * M_outbuf;
+    int M_remained;
+    char_type M_remained_char;
+    ConnType M_connect;
 
-        public:
-            SocketStreamBuf(Socket &socket,
-                            const Addr &dest,
-                            ConnType conn = CONN_ON_READ,
-                            std::streamsize bufsize = 8192);
+    // not used
+    SocketStreamBuf( const SocketStreamBuf & );
+    // not used
+    SocketStreamBuf & operator=( const SocketStreamBuf & );
 
-            SocketStreamBuf(Socket &socket,
-                            ConnType conn = NO_CONN,
-                            std::streamsize bufsize = 8192);
+public:
+    SocketStreamBuf( Socket & socket,
+                     const Addr & dest,
+                     ConnType conn = CONN_ON_READ,
+                     std::streamsize bufsize = 8192 );
 
-            virtual ~SocketStreamBuf();
+    SocketStreamBuf( Socket & socket,
+                     ConnType conn = NO_CONN,
+                     std::streamsize bufsize = 8192 );
 
-            void setEndPoint(const Addr &addr)
-            {
-                M_end_point = addr;
-            }
+    virtual
+    ~SocketStreamBuf();
 
-            void setConnectType(ConnType conn)
-            {
-                M_connect = conn;
-            }
+    void setEndPoint( const Addr & addr )
+      {
+          M_end_point = addr;
+      }
 
-        private:
-            bool writeData();
+    void setConnectType( ConnType conn )
+      {
+          M_connect = conn;
+      }
 
-        protected:
-            virtual int_type overflow(int_type c = traits_type::eof());
+private:
 
-            virtual int sync();
+    bool writeData();
 
-            virtual int_type underflow();
-        };
+protected:
 
-    }
+    virtual
+    int_type overflow( int_type c = traits_type::eof() );
+
+    virtual
+    int sync();
+
+    virtual
+    int_type underflow();
+
+};
+
+}
 }
 
 #endif
